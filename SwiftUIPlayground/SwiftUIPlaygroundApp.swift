a69809d37314e7868e2fe617606594fa177db3a8
Moamen Hassaballah <hassaballahmoamen84@gmail.com>
2024-08-13 19:39:05 +0300

    Initial Commit

---
+++ SwiftUIPlayground/SwiftUIPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPlaygroundApp.swift
+//  SwiftUIPlayground
+//
+//  Created by Moamen Hassaballah on 13/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
